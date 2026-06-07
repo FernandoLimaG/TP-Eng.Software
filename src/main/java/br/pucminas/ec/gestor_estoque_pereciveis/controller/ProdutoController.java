1643516533ce3b09048e8f58fdcb47ab7d559440
Fernando de Lima Gonçalves <fernando.limag@outlook.com>
2026-06-07 15:03:32 -0300

    Implementacao de edicao de produto

--- src/main/java/br/pucminas/ec/gestor_estoque_pereciveis/controller/ProdutoController.java
+++ src/main/java/br/pucminas/ec/gestor_estoque_pereciveis/controller/ProdutoController.java
@@ -16,29 +16,56 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @GetMapping("/produtos")
-    public String listar(Model model) {
-
-        model.addAttribute("produtos", produtoRepository.findAll());
-
-        return "produtos";
-    }
-
-    @GetMapping("/produtos/deletar/{id}")
-    public String deletar(@PathVariable Long id) {
-        produtoRepository.deleteById(id);
-        return "redirect:/produtos";
-    }
-
     @PostMapping("/produtos")
     public String salvar(Produto produto) {
-        // --Validacao basica de entrada do usuario (nao possui alerta de erro ainda, apenhas falha)
+        produto.setId(null);
+
         boolean validacaoNome =  produto.getNome() != null && !produto.getNome().trim().isEmpty();
         boolean validacaoDataValidade = !produto.getValidade().isBefore(LocalDate.now());
         boolean validacaoQuantidade = produto.getQuantidade() > 0;
         if(validacaoNome && validacaoDataValidade && validacaoQuantidade){
             produtoRepository.save(produto);
         }
+        return "redirect:/produtos";
+    }
+
+    @GetMapping("/produtos")
+    public String listar(Model model) {
+        model.addAttribute("produtos", produtoRepository.findAll());
+        model.addAttribute("produto", new Produto());
+        return "produtos";
+    }
+
+    @GetMapping("/produtos/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        Produto produto = produtoRepository.findById(id).orElse(null);
+
+        if (produto == null) {
+            return "redirect:/produtos";
+        }
+
+        model.addAttribute("produtos",  produtoRepository.findAll());
+        model.addAttribute("produto", produto);
+
+        return  "produtos";
+    }
+
+    @PostMapping("/produtos/atualizar")
+    public String atualizar(Produto produto) {
+        boolean validacaoNome =  produto.getNome() != null && !produto.getNome().trim().isEmpty();
+        boolean validacaoDataValidade = !produto.getValidade().isBefore(LocalDate.now());
+        boolean validacaoQuantidade = produto.getQuantidade() > 0;
+
+        if(produto.getId() != null && validacaoNome && validacaoDataValidade && validacaoQuantidade){
+            produtoRepository.save(produto);
+        }
+
+        return "redirect:/produtos";
+    }
+
+    @GetMapping("/produtos/deletar/{id}")
+    public String deletar(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
         return "redirect:/produtos";
     }
 }
